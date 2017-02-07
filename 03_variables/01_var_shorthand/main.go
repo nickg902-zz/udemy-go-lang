60e3842a608b4d1bb09826056a83a00bc9692632
nickg902 <github@cipherspace.ca>
2017-02-06 22:17:35 -0400

    var examples

---
+++ 03_variables/01_var_shorthand/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 42
+	b := "nickg"
+	c := 42.42
+	d := true
+
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+
+	//prints type
+	fmt.Printf("%T \n", a)
+	fmt.Printf("%T \n", b)
+	fmt.Printf("%T \n", c)
+	fmt.Printf("%T \n", d)
+}
